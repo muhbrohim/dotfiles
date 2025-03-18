a9cf5ab6579cbadb1c5af2c243a407b5c5646399
brohim <brohim.dev@gmail.com>
2025-03-19 00:29:15 +0700

    need to run chmod +x first, and try run launch.sh again

--- linux/polybar/.config/polybar/launch.sh
+++ linux/polybar/.config/polybar/launch.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -e
 
 # Kill existing Polybar instances
-pkill -x polybar
+pkill -x polybar || true
 
 # Wait for Polybar to exit
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar on all monitors
-if type "xrandr"; then
+if command -v xrandr > /dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    MONITOR=$m polybar --reload toph >> /tmp/polybar_$m.log 2>&1 &
   done
 else
-  polybar --reload toph &
+  polybar --reload toph >> /tmp/polybar_single.log 2>&1 &
 fi
 
